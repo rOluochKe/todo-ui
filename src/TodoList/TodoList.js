fddfd0565ff026576dcc102e600887c2a045b15c
Raymond Oluoch <rasholuoch@gmail.com>
2021-03-10 01:31:24 +0300

    adding fetching data and posting to the database

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import TodoForm from './TodoForm';
+import TodoItem from './TodoItem';
 
 const api_url = `http://localhost:3001/api/v1/todos`;
 
@@ -10,15 +11,40 @@ class TodoList extends Component {
     this.state = {
       items: []
     }
+    this.updateTodoList = this.updateTodoList.bind(this);
+  }
+
+  componentDidMount() {
+    this.getTasks();
+  }
+
+  getTasks() {
+    fetch(api_url)
+    .then(response => response.json())
+    .then(response_items => {
+      this.setState({
+        items: response_items.reverse
+      })
+    });
+  }
+
+  updateTodoList(item) {
+    let _items = this.state.items
+    // unshift add to the beginning of the array
+    _items.unshift(item)
+    this.setState({
+      items: _items
+    })
   }
 
   render() {
     return (
       <div>
-        <TodoForm />
-        <ul>
-          <li>Todo #1</li>
-          <li>Todo #2</li>
+        <TodoForm api_url={api_url} updateTodoList={this.updateTodoList} />
+        <ul id="todo_list">
+          {this.state.items.map((item) => (
+            <TodoItem key={item.id} item={item} />
+          ))}
         </ul>
       </div>
     )
