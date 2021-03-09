630e9e49ce596c6d0f1742797372cdf2f9bfc7bf
Raymond Oluoch <rasholuoch@gmail.com>
2021-03-10 00:06:29 +0300

    projejct setup and structure

---
+++ src/TodoList/TodoForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class TodoForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Todo Form</h3>
+      </div>
+    )
+  }
+}
+
+export default TodoForm;
