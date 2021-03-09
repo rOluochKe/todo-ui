7da43cc11b77a8d679d3de85129332e81c06038f
Raymond Oluoch <rasholuoch@gmail.com>
2021-03-10 02:03:04 +0300

    deleting records and improing the app design

--- src/TodoList/TodoItem.js
+++ src/TodoList/TodoItem.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import Grid from "@material-ui/core/Grid"
+import Paper from "@material-ui/core/Paper"
+import { makeStyles } from "@material-ui/core/styles"
+import Button from "@material-ui/core/Button"
+import DeleteIcon from "@material-ui/icons/Delete"
+
+const useStyles = makeStyles({
+  root: {
+    height: "3em",
+    padding: "2em",
+    margin: "1em"
+  }
+})
 
 export default function TodoItem(props) {
+  const classes = useStyles();
+  function handleDelete() {
+      props.deleteItem(props.item)
+  }
+  
   return (
-    <li>{props.item.id}: {props.item.task}</li>
+    <Grid container spacing={0}>
+      <Grid item xs={9}>
+        <Paper elevation={3} className={classes.root}>
+            {props.item.task}
+            <hr />
+            <Button
+                variant="contained"
+                color="secondary"
+                size="small"
+                startIcon={<DeleteIcon />}
+                onClick={handleDelete}>
+                Delete
+            </Button>
+        </Paper>
+      </Grid>
+    </Grid>
   )
 }
--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import TodoForm from './TodoForm';
 import TodoItem from './TodoItem';
+import Grid from '@material-ui/core/Grid';
 
 const api_url = `http://localhost:3001/api/v1/todos`;
 
@@ -12,6 +13,7 @@ class TodoList extends Component {
       items: []
     }
     this.updateTodoList = this.updateTodoList.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
 
   componentDidMount() {
@@ -37,16 +39,39 @@ class TodoList extends Component {
     })
   }
 
+  deleteItem(item) {
+    // delete the item remotely
+    // localhost:3001/api/v1/todos + /13
+    var deleteURL = api_url + `/${item.id}`
+    fetch(deleteURL, {
+        method: "DELETE"
+    }).then(() => {
+        // Client side delete
+        var _items = this.state.items;
+        var index = _items.indexOf(item)
+        _items.splice(index, 1);
+        this.setState({
+            items: _items
+        })
+    })
+  }
+
   render() {
     return (
-      <div>
-        <TodoForm api_url={api_url} updateTodoList={this.updateTodoList} />
-        <ul id="todo_list">
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+            <TodoForm api_url={api_url} updateTodoList={this.updateTodoList} />
+        </Grid>
+
+        <Grid item xs={12} id="todo_list">
             {this.state.items.map((item) => (
-                <TodoItem key={item.id} item={item} />
+                <TodoItem
+                    key={item.id}
+                    item={item}
+                    deleteItem={this.deleteItem} />
             ))}
-        </ul>
-      </div>
+        </Grid>
+      </Grid>
     )
   }
 }
